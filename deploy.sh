e5be2a51c37345009ce18d537b16da0812e5b970
whlshdw <whlshdw@gmail.com>
2021-02-05 11:48:17 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,7 @@
 
 # 当发生错误时中止脚本
 set -e
-hexo clean&&hexo g
-git init
-#git checkout -b main
-git add -A
-git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:whlshdw/whlshdw.github.io.git main
-
+#将index.html等文件部署到github中，通过 whlshdw.github.io 或者暂时用：blog.sthwhl.com 访问
+hexo clean && hexo g && hexo d
+#退出
 cd -
